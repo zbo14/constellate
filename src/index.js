0fcaffc24c16134351f2b4342b24965bc485349f
k23Burke <k23burke@gmail.com>
2017-10-26 15:39:56 -0700

    build for im[rt into projects

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const MetadataService = require('./metadata-service');
+const ContentService = require('./content-service');
+const ContentServiceBrowser = require('./content-service/browser');
+
+module.exports = {
+  MetadataService,
+  ContentService,
+  ContentServiceBrowser,
+};
