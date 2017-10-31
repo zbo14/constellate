b21e4e94d87e7a81997cc4c669e8d16fff37f1ca
k23Burke <k23burke@gmail.com>
2017-10-31 13:29:20 -0400

    complete

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,23 @@
-// const gulp = require('gulp');
-// const babel = require('gulp-babel');
-// const browserify = require('browserify');
-// const source = require('vinyl-source-stream');
-// const buffer = require('vinyl-buffer');
-// const uglify = require('gulp-uglify');
-// const gutil = require('gulp-util');
-
-// gulp.task('default', function () {
-//   // set up the browserify instance on a task basis
-//   var b = browserify({
-//     entries: './src/index.js',
-//     debug: true
-//   });
-
-//   return b.bundle()
-//     .pipe(source('index.js'))
-//     .pipe(buffer())
-//     .pipe(babel({ presets: ['env', 'es2015'] }))
-//     // .pipe(uglify())
-//     .on('error', gutil.log)
-//     .pipe(gulp.dest('./build/'));
-// });
-
-
 const gulp = require('gulp');
 const babel = require('gulp-babel');
-// const concat = require('gulp-concat');
+const browserify = require('browserify');
+const source = require('vinyl-source-stream');
+const buffer = require('vinyl-buffer');
+const uglify = require('gulp-uglify');
+const gutil = require('gulp-util');
 
-gulp.task('default', () => {
-  gulp.src('./src/**/*.js')
-    .pipe(babel({ presets: ['env'] }))
-    .pipe(gulp.dest('./lib/'));
-  }
-);
+gulp.task('default', function () {
+  // set up the browserify instance on a task basis
+  var b = browserify({
+    entries: './src/index.js',
+    debug: true
+  });
+
+  return b.bundle()
+    .pipe(source('index.js'))
+    .pipe(buffer())
+    .pipe(babel({ presets: ['env', 'es2015'] }))
+    .pipe(uglify())
+    .on('error', gutil.log)
+    .pipe(gulp.dest('./build/'));
+});
